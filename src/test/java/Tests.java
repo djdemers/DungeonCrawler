869a3847a3a76ed9f85bdcbdaf9df5755c8b4a6d
djdemers <djdemers@asu.edu>
2024-06-18 19:16:42 -0700

    added test

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Tests {
+//Static imports at the top
+// Regular imports below static imports
+// Both groups sorted alphabetically
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class Tests {
+    @Test
+    public void testGreeting(){
+        Main classTest = new Main();
+        assertEquals("Hello, world!", classTest.getGreeting());
+    }
+
 }
