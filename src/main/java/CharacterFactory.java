41ae01d57689c9e39c302a919f02f7eae18dd9ca
djdemers <djdemers@asu.edu>
2024-06-20 16:41:40 -0700

    added simple CharacterFactory

---
+++ src/main/java/CharacterFactory.java
@@ -0,0 +1,16 @@
+public class CharacterFactory {
+    public static Character createCharacter(String type, String name, Race race) {
+        switch (type.toLowerCase()) {
+            case "warrior":
+                return new CharWarrior(name, race);
+            case "mage":
+                return new CharMage(name, race);
+            case "archer":
+                return new CharArcher(name, race);
+            case "rogue":
+                return new CharRogue(name,race);
+            default:
+                throw new IllegalArgumentException("Unknown character type: " + type);
+        }
+    }
+}
