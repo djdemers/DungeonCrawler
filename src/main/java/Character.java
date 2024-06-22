c111e29bc0ac7d1175e870b83abeab46c090bec8
Your Name <djdemers@asu.edu>
2024-06-21 18:30:06 -0700

    made simple Character class

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Character {
+public abstract class Character {
+    protected String name;
+    protected int health;
+    protected int attackPower;
+
+    public Character(String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    public void attack(Enemy target) {
+        target.reduceHealth(this.attackPower);
+        System.out.println(this.name + " attacks " + target.getName() + " for " + this.attackPower + " damage.");
+    }
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+    public void reduceHealth(int damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
+
+    public void displayStats() {
+        System.out.println(name + " - Health: " + health + ", Attack Power: " + attackPower);
+    }
+    public String getName(){
+        return this.name;
+    }
 }
