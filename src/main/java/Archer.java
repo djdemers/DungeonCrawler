f2f4b148c9c69ad4d39a7cb4e9c8018426fdeeaa
Your Name <djdemers@asu.edu>
2024-06-21 18:32:00 -0700

    made simple Archer class

--- src/main/java/Archer.java
+++ src/main/java/Archer.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Archer {
+public class Archer extends Character {
+    public Archer(String name) {
+        super(name, 120, 25);  // Archer has balanced health and attack, with range advantage
+    }
 }
