d3b961a614cc13c6f47e1bc46e4af0b60e086c34
Your Name <djdemers@asu.edu>
2024-06-21 18:31:26 -0700

    made simple Mage class

--- src/main/java/Mage.java
+++ src/main/java/Mage.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Mage {
+public class Mage extends Character {
+    public Mage(String name) {
+        super(name, 100, 40);  // Mage has less health but higher magical attack power
+    }
 }
+
