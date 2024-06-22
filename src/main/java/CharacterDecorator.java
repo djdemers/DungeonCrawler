c7bcbb16dc5d5a50631727fd15315aeccc304dbb
Your Name <djdemers@asu.edu>
2024-06-21 18:33:45 -0700

    made simple CharacterDecorator class

--- src/main/java/CharacterDecorator.java
+++ src/main/java/CharacterDecorator.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class CharacterDecorator {
+public abstract class CharacterDecorator extends Character {
+    protected Character decoratedCharacter;
+
+    public CharacterDecorator(Character decoratedCharacter) {
+        super(decoratedCharacter.name, decoratedCharacter.health,
+                decoratedCharacter.attackPower);
+        this.decoratedCharacter = decoratedCharacter;
+    }
+
+    @Override
+    public void displayStats() {
+        decoratedCharacter.displayStats();
+    }
+
+    @Override
+    public void attack(Enemy target) {
+        decoratedCharacter.attack(target);
+    }
+
+    @Override
+    public void reduceHealth(int damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
 }
+
+
+
