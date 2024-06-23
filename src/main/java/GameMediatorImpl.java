e6f7b3c7c45afe74a754d20947d5e9a6b19ff2ed
djdemers <djdemers@asu.edu>
2024-06-23 09:00:33 -0700

    implemented a simple Game Mediator

--- src/main/java/GameMediatorImpl.java
+++ src/main/java/GameMediatorImpl.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class GameMediatorImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameMediatorImpl implements GameMediator {
+    private Character hero;
+    private List<Enemy> enemies;
+    private int currentLevel = 1;
+
+    public GameMediatorImpl(Character hero) {
+        this.hero = hero;
+        this.enemies = new ArrayList<>();
+    }
+
+    @Override
+    public void fight(Character hero, Enemy enemy) {
+        while (hero.isAlive() && enemy.isAlive()) {
+            hero.attackEnemy(enemy);
+            if (enemy.isAlive()) {
+                enemy.attackHero(hero);
+            }
+        }
+        if (!hero.isAlive()) {
+            gameOver();
+        } else {
+            System.out.println(hero.getName() + " has defeated " + enemy.getName() + "!");
+            //nextLevel(hero);
+        }
+    }
+
+    @Override
+    public void nextLevel(Character hero) {
+        currentLevel++;
+        System.out.println("Welcome to level " + currentLevel);
+        Enemy nextEnemy = EnemyFactory.createEnemy("next_level_enemy_type");
+        enemies.add(nextEnemy);
+        fight(hero, nextEnemy);
+    }
+
+    @Override
+    public void gameOver() {
+        System.out.println("Game Over. " + hero.getName() + " reached level " + currentLevel + ".");
+    }
 }
