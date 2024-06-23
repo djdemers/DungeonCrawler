6b88f14f45613c70509f5589461b761c601d41fe
djdemers <djdemers@asu.edu>
2024-06-23 12:30:12 -0700

    ran simple tests to observe random generation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,21 @@
+import characters.Character;
+import characters.decorators.SwordDecorator;
+import enemies.Enemy;
+import factories.CharacterFactory;
+import factories.EnemyFactory;
+
 public class Main {
 
     public static void main(String[] args) {
         Main app = new Main();
         System.out.println(app.getGreeting());
 
-        Character hero = new CharRogue("Sneak", Race.FAIRY);
+        Character hero = CharacterFactory.createRandomCharacter("Hero");
+        System.out.println(hero.getClass().getSimpleName() + " is the class you have been assigned");
+        System.out.println(hero.getRace().getName() + " is the race that you have been assigned");
         hero.displayStats();
         GameMediator mediator = new GameMediatorImpl(hero);
-        Enemy initialEnemy = EnemyFactory.createEnemy("goblin");
+        Enemy initialEnemy = EnemyFactory.createRandomSmallEnemy();
         mediator.fight(hero,initialEnemy);
 
 
