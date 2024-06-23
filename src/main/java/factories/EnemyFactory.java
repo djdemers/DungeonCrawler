b6416b446ac7630fdec3b93c78a09f947225f41c
djdemers <djdemers@asu.edu>
2024-06-23 12:25:18 -0700

    modified EnemyFactory to work with EnemyType to generate random small/med/boss enemies

--- src/main/java/factories/EnemyFactory.java
+++ src/main/java/factories/EnemyFactory.java
@@ -1,16 +1,38 @@
-//Factory Design Pattern from Creational Patterns
+package factories;
+
+import enemies.*;
+import enemies.Enemy;
 
 public class EnemyFactory {
-    public static Enemy createEnemy(String type) {
-        switch (type.toLowerCase()) {
-            case "goblin":
+
+    public static Enemy createEnemy(EnemyType type) {
+        switch (type) {
+            case GOBLIN:
                 return new Goblin();
-            case "troll":
+            case RAT:
+                return new Rat();
+            case SPIDER:
+                return new Spider();
+            case TROLL:
                 return new Troll();
-            case "dragon":
+            case DRAGON:
                 return new Dragon();
             default:
                 throw new IllegalArgumentException("Unknown enemy type: " + type);
         }
     }
+
+    public static Enemy createRandomSmallEnemy() {
+        return createEnemy(EnemyType.getRandomSmallEnemy());
+    }
+
+    public static Enemy createRandomMediumEnemy() {
+        return createEnemy(EnemyType.getRandomMediumEnemy());
+    }
+
+    public static Enemy createRandomBoss() {
+        return createEnemy(EnemyType.getRandomBossEnemy());
+    }
 }
+
+
