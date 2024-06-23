9c5103ef7c81ea7813f8af1623bdf301109bd69c
djdemers <djdemers@asu.edu>
2024-06-23 12:23:25 -0700

    created EnemyType class to assist with random enemy generation

--- src/main/java/enemies/EnemyType.java
+++ src/main/java/enemies/EnemyType.java
@@ -1,2 +1,23 @@
-package enemies;public class EnemyType {
+package enemies;
+
+public enum EnemyType {
+        GOBLIN, RAT, SPIDER, // Small enemies
+        TROLL,               // Medium enemies
+        DRAGON;              // Boss enemies
+
+        public static EnemyType getRandomSmallEnemy() {
+                EnemyType[] small = {GOBLIN, RAT, SPIDER};
+                return small[(int) (Math.random() * small.length)];
+        }
+
+        public static EnemyType getRandomMediumEnemy() {
+
+                return TROLL;
+        }
+
+        public static EnemyType getRandomBossEnemy() {
+
+                return DRAGON;
+        }
 }
+
