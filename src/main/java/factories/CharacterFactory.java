5594decd836327eae34598a8a9b1750ad802a72d
djdemers <djdemers@asu.edu>
2024-06-24 11:56:50 -0700

    added javadoc

--- src/main/java/factories/CharacterFactory.java
+++ src/main/java/factories/CharacterFactory.java
@@ -1,9 +1,26 @@
 package factories;
 
-import characters.*;
 import characters.Character;
+import characters.CharacterClass;
+import characters.Race;
+import characters.Warrior;
+import characters.Mage;
+import characters.Rogue;
+import characters.Archer;
 
+/**
+ * Factory class for creating character instances.
+ * This class provides a static method to create characters with randomly assigned classes and races.
+ */
 public class CharacterFactory {
+
+    /**
+     * Creates a character with a randomly selected class and race.
+     *
+     * @param name The name of the character to be created.
+     * @return A new instance of a character with specified name, random class, and random race.
+     * @throws IllegalArgumentException If the randomly selected character class is unknown.
+     */
     public static Character createRandomCharacter(String name) {
         Race race = Race.getRandomRace();
         CharacterClass charClass = CharacterClass.getRandomClass();
