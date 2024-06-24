f22221d1c25a95e6565a3ac1d798653a54ac89c4
djdemers <djdemers@asu.edu>
2024-06-24 11:56:00 -0700

    added javadoc

--- src/main/java/factories/EnemyFactory.java
+++ src/main/java/factories/EnemyFactory.java
@@ -1,10 +1,20 @@
 package factories;
 
 import enemies.*;
-import enemies.Enemy;
 
+/**
+ * Factory class for creating enemies. This class abstracts the instantiation
+ * of different types of enemies based on the specified enemy type.
+ */
 public class EnemyFactory {
 
+    /**
+     * Creates an enemy of the specified type.
+     *
+     * @param type The type of enemy to create.
+     * @return The newly created enemy object.
+     * @throws IllegalArgumentException if the provided type is not recognized.
+     */
     public static Enemy createEnemy(EnemyType type) {
         switch (type) {
             case GOBLIN:
@@ -22,17 +32,31 @@ public class EnemyFactory {
         }
     }
 
+    /**
+     * Creates a random small enemy.
+     *
+     * @return A new instance of a random small enemy.
+     */
     public static Enemy createRandomSmallEnemy() {
         return createEnemy(EnemyType.getRandomSmallEnemy());
     }
 
+    /**
+     * Creates a random medium enemy.
+     *
+     * @return A new instance of a random medium enemy.
+     */
     public static Enemy createRandomMediumEnemy() {
         return createEnemy(EnemyType.getRandomMediumEnemy());
     }
 
+    /**
+     * Creates a random boss enemy.
+     *
+     * @return A new instance of a random boss enemy.
+     */
     public static Enemy createRandomBoss() {
         return createEnemy(EnemyType.getRandomBossEnemy());
     }
 }
-
 
