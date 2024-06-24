c1f3f3959bcf975b18dba62b52de1807d442fce6
djdemers <djdemers@asu.edu>
2024-06-24 12:02:53 -0700

    added javadoc

--- src/main/java/characters/Race.java
+++ src/main/java/characters/Race.java
@@ -1,42 +1,90 @@
 package characters;
 
+/**
+ * Enum representing the various races available in the game.
+ * Each race has unique bonuses to health, agility, and strength.
+ */
 public enum Race {
-    HUMAN("Human", 5, 5, 5),
-    ELF("Elf", 4, 6, 7),
-    DWARF("Dwarf", 8, 3, 4),
-    ORC("Orc", 10, 2, 3),
-    FAIRY("Fairy", 3, 10,4);
+    HUMAN("Human", 2, 2, 2, 0),
+    ELF("Elf", 1, 3, 2, 0),
+    DWARF("Dwarf", 3, 2, 1, 0),
+    ORC("Orc", 1, 4, 1, 0),
+    FAIRY("Fairy", 1, 0, 5, 0);
 
     private final String name;
     private final int healthBonus;
     private final int agilityBonus;
     private final int strengthBonus;
+    private final int defenseBonus;
 
-
-    Race(String name, int healthBonus, int agilityBonus, int strengthBonus) {
+    /**
+     * Constructs a race with specified name and attribute bonuses.
+     *
+     * @param name the name of the race
+     * @param healthBonus the health bonus for the race
+     * @param agilityBonus the agility bonus for the race
+     * @param strengthBonus the strength bonus for the race
+     */
+    Race(String name, int healthBonus, int strengthBonus, int agilityBonus, int defenseBonus) {
         this.name = name;
         this.healthBonus = healthBonus;
-        this.agilityBonus = agilityBonus;
         this.strengthBonus = strengthBonus;
+        this.agilityBonus = agilityBonus;
+        this.defenseBonus = defenseBonus;
     }
 
+    /**
+     * Returns the name of the race.
+     *
+     * @return the name of the race
+     */
     public String getName() {
         return name;
     }
 
-    public static Race getRandomRace(){
+    /**
+     * Returns a random race from the defined races.
+     *
+     * @return a randomly selected race
+     */
+    public static Race getRandomRace() {
         return values()[(int) (Math.random() * values().length)];
     }
 
+    /**
+     * Returns the health bonus of the race.
+     *
+     * @return the health bonus
+     */
     public int getHealthBonus() {
         return healthBonus;
     }
 
+    /**
+     * Returns the agility bonus of the race.
+     *
+     * @return the agility bonus
+     */
     public int getAgilityBonus() {
         return agilityBonus;
     }
 
+    /**
+     * Returns the strength bonus of the race.
+     *
+     * @return the strength bonus
+     */
     public int getStrengthBonus() {
         return strengthBonus;
     }
+
+    /**
+     * Returns the defense bonus of the race.
+     *
+     * @return the defense bonus
+     */
+    public int getDefenseBonus(){
+        return defenseBonus;
+    }
 }
+
