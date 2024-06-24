ebe49785e0ba5bcb058d70d509762f4e51a7c57c
djdemers <djdemers@asu.edu>
2024-06-24 12:15:22 -0700

    ran a few tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
 import characters.Character;
+import characters.Race;
+import characters.Warrior;
+import characters.decorators.ArmorDecorator;
+import characters.decorators.CharacterDecorator;
+import characters.decorators.DaggerDecorator;
 import characters.decorators.SwordDecorator;
 import enemies.Enemy;
 import factories.CharacterFactory;
@@ -9,19 +14,38 @@ public class Main {
     public static void main(String[] args) {
         Main app = new Main();
         System.out.println(app.getGreeting());
-
         Character hero = CharacterFactory.createRandomCharacter("Hero");
         System.out.println(hero.getClass().getSimpleName() + " is the class you have been assigned");
         System.out.println(hero.getRace().getName() + " is the race that you have been assigned");
+        System.out.println("Before any decoration:");
         hero.displayStats();
+
+        //hero = new SwordDecorator(hero, 1);
+        //System.out.println("After applying SwordDecorator:");
+        //hero.displayStats();
+
+        //((SwordDecorator) hero).unequip();
+        //System.out.println("After removing ArmorDecorator:");
+        //hero.displayStats();
+
+        //hero = new ArmorDecorator(hero, 0);
+        //System.out.println("After applying ArmorDecorator:");
+        //hero.displayStats();
+
+        //hero = new DaggerDecorator(hero, 1, 2);
+        //System.out.println("After applying DaggerDecorator:");
+        //hero.displayStats();
+
+
+
+
         GameMediator mediator = new GameMediatorImpl(hero);
         Enemy initialEnemy = EnemyFactory.createRandomSmallEnemy();
         mediator.fight(hero,initialEnemy);
 
 
 
-       hero = new SwordDecorator(hero, 10);
-       hero.displayStats();
+
 
     }
 
