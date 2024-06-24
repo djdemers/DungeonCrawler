f994b8973d7f815897d0ea67e729b6333e312968
djdemers <djdemers@asu.edu>
2024-06-24 12:09:35 -0700

    added javadoc and adjusted methods to properly override

--- src/main/java/characters/decorators/DaggerDecorator.java
+++ src/main/java/characters/decorators/DaggerDecorator.java
@@ -1,2 +1,33 @@
-package characters.decorators;public class DaggerDecorator {
+package characters.decorators;
+import characters.Character;
+
+/**
+ * Decorator that enhances a character's attack power and speed by equipping a dagger.
+ */
+public class DaggerDecorator extends CharacterDecorator {
+    private int additionalAttackPower;
+    private int additionalSpeed;
+
+    public DaggerDecorator(Character decoratedCharacter, int additionalAttackPower, int additionalSpeed) {
+        super(decoratedCharacter);
+        this.additionalAttackPower = additionalAttackPower;
+        this.additionalSpeed = additionalSpeed;
+    }
+
+    @Override
+    public int getAttackPower() {
+        // Adds additional attack power to the decorated character's attack power
+        return super.getAttackPower() + additionalAttackPower;
+    }
+
+    @Override
+    public int getSpeed() {
+        // Adds additional speed to the decorated character's speed
+        return super.getSpeed() + additionalSpeed;
+    }
+    @Override
+    public void unequip() {
+        super.setAttackPower(super.getAttackPower() - additionalAttackPower);
+        super.setSpeed(super.getSpeed() - additionalSpeed);
+    }
 }
