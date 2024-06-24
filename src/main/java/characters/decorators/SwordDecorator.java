d875d38790d5c57e63ef764eaf10dac8404a46ba
djdemers <djdemers@asu.edu>
2024-06-24 12:10:13 -0700

    removed unused method

--- src/main/java/characters/decorators/SwordDecorator.java
+++ src/main/java/characters/decorators/SwordDecorator.java
@@ -1,29 +1,28 @@
 package characters.decorators;
-
 import characters.Character;
-import enemies.Enemy;
 
+/**
+ * Decorator that enhances a character's attack power due to equipping a sword.
+ */
 public class SwordDecorator extends CharacterDecorator {
     private int additionalAttackPower;
 
     public SwordDecorator(Character decoratedCharacter, int additionalAttackPower) {
         super(decoratedCharacter);
         this.additionalAttackPower = additionalAttackPower;
-        this.decoratedCharacter.setAttackPower(decoratedCharacter.getAttackPower() + additionalAttackPower);
+    }
+    @Override
+    public int getAttackPower() {
+        return super.getAttackPower() + additionalAttackPower;
     }
 
     @Override
-    public void attackEnemy(Enemy target) {
-        super.attackEnemy(target);
+    public void unequip() {
+        super.setAttackPower(super.getAttackPower() - additionalAttackPower);
     }
 
-
-    public void removeSword() {
-        this.decoratedCharacter.setAttackPower(decoratedCharacter.getAttackPower() - additionalAttackPower);
-    }
-
-    public int getAdditionalAttackPower(){
-        return additionalAttackPower;
-    }
 }
+
+
+
 
