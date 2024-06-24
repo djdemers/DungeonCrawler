a11cedb9c07e6caafb9046d5d1517f7769f1be75
djdemers <djdemers@asu.edu>
2024-06-24 12:06:13 -0700

    added javadoc and adjusted methods to properly override

--- src/main/java/characters/decorators/ArmorDecorator.java
+++ src/main/java/characters/decorators/ArmorDecorator.java
@@ -1,2 +1,31 @@
-package characters.decorators;public class ArmorDecorator {
+package characters.decorators;
+
+import characters.Character;
+
+/**
+ * Decorator that enhances a character's defense due to equipping armor.
+ */
+public class ArmorDecorator extends CharacterDecorator {
+    private int additionalDefense;
+
+    public ArmorDecorator(Character decoratedCharacter, int additionalDefense) {
+        super(decoratedCharacter);
+        this.additionalDefense = additionalDefense;
+    }
+
+    @Override
+    public int getDefense() {
+        // Add the armor's additional defense to the decorated character's defense
+        return super.getDefense() + additionalDefense;
+    }
+
+    @Override
+    public void unequip() {
+        super.setDefense(super.getDefense() - additionalDefense);
+    }
+    @Override
+    public void reduceHealth(int damage) {
+        // Apply the enhanced defense when taking damage
+        super.reduceHealth(Math.max(0, damage - additionalDefense));
+    }
 }
