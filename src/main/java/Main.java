4952335acc520f95ac4297763d0f03622e1eed8c
djdemers <djdemers@asu.edu>
2024-06-25 15:26:56 -0700

    made some test scenarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 import characters.Character;
-import characters.Race;
-import characters.Warrior;
-import characters.decorators.ArmorDecorator;
+import characters.decorators.Armor;
 import characters.decorators.CharacterDecorator;
-import characters.decorators.DaggerDecorator;
-import characters.decorators.SwordDecorator;
+import characters.decorators.Dagger;
+import characters.decorators.Sword;
 import enemies.Enemy;
 import factories.CharacterFactory;
 import factories.EnemyFactory;
@@ -14,29 +12,38 @@ public class Main {
     public static void main(String[] args) {
         Main app = new Main();
         System.out.println(app.getGreeting());
+
+        //Create random generated Character
         Character hero = CharacterFactory.createRandomCharacter("Hero");
         System.out.println(hero.getClass().getSimpleName() + " is the class you have been assigned");
         System.out.println(hero.getRace().getName() + " is the race that you have been assigned");
+        //Initial Stats
         System.out.println("Before any decoration:");
         hero.displayStats();
 
-        //hero = new SwordDecorator(hero, 1);
-        //System.out.println("After applying SwordDecorator:");
-        //hero.displayStats();
+        // Create item decorators
+        CharacterDecorator sword = new Sword(hero, 5);
+        CharacterDecorator armor = new Armor(hero, 3);
+        CharacterDecorator dagger = new Dagger(hero, 1, 2);
 
-        //((SwordDecorator) hero).unequip();
-        //System.out.println("After removing ArmorDecorator:");
-        //hero.displayStats();
+        // Equip items
+        hero.equipDecorator(sword);
+        System.out.println("After equipping sword:");
+        hero.displayStats();
 
-        //hero = new ArmorDecorator(hero, 0);
-        //System.out.println("After applying ArmorDecorator:");
-        //hero.displayStats();
+        hero.equipDecorator(armor);
+        System.out.println("After equipping armor:");
+        hero.displayStats();
 
-        //hero = new DaggerDecorator(hero, 1, 2);
-        //System.out.println("After applying DaggerDecorator:");
-        //hero.displayStats();
+        // Remove sword
+        hero.unequipDecorator(Sword.class);
+        System.out.println("After removing sword:");
+        hero.displayStats();
 
-
+        //Equip Dagger
+        hero.equipDecorator(dagger);
+        System.out.println("After equipping dagger:");
+        hero.displayStats();
 
 
         GameMediator mediator = new GameMediatorImpl(hero);
