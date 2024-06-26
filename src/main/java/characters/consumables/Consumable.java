39b27a4cde33e1d82101de12ec6a0d573d833e64
djdemers <djdemers@asu.edu>
2024-06-25 19:01:46 -0700

    added an abstract Consumable class for single use items

--- src/main/java/characters/consumables/Consumable.java
+++ src/main/java/characters/consumables/Consumable.java
@@ -1,2 +1,30 @@
-package characters.consumables;public class Consumable {
+package characters.consumables;
+import characters.Character;
+
+/**
+ * Abstract class representing a consumable item in the game.
+ * Consumables are items that can be used once to affect a character,
+ * typically providing healing, buffing, or other effects.
+ */
+public abstract class Consumable {
+    protected String name;
+    protected String description;
+
+    /**
+     * Constructs a new Consumable item with the specified name and description.
+     *
+     * @param name The name of the consumable item.
+     * @param description A brief description of the consumable item.
+     */
+    public Consumable(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    /**
+     * Uses the consumable item on a specified character, applying its effects.
+     *
+     * @param character The character on whom the consumable will be used.
+     */
+    public abstract void use(Character character);
 }
