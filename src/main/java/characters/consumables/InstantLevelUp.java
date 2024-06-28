9b3c55d16c732d2a0d4fae0a5f779d189a3e1f92
djdemers <djdemers@asu.edu>
2024-06-28 11:51:32 -0700

    Still need to implement logic for InstantLevelUp and MagicScroll

--- src/main/java/characters/consumables/InstantLevelUp.java
+++ src/main/java/characters/consumables/InstantLevelUp.java
@@ -1,2 +1,21 @@
-package characters.consumables;public class InstantLevelUp {
+package characters.consumables;
+
+import characters.Character;
+import service.SingleUse;
+
+public class InstantLevelUp extends Consumable implements SingleUse {
+    public InstantLevelUp(String name, String description, int levelIncrement){
+        super(name, description);
+    }
+    @Override
+    public void use(Character character){
+        //character.levelUp();
+        System.out.println(character.getName() + " uses " + name + " and levels up instantly!");
+    }
+    public String getName(){
+        return super.name;
+    }
+    public String getDescription(){
+        return super.description;
+    }
 }
--- src/main/java/characters/consumables/MagicScroll.java
+++ src/main/java/characters/consumables/MagicScroll.java
@@ -1,2 +1,26 @@
-package characters.consumables;public class MagicScroll {
+package characters.consumables;
+
+import characters.Character;
+import service.SingleUse;
+
+// PLACE HOLDER CLASS
+public class MagicScroll extends Consumable implements SingleUse {
+    private int power;
+
+    public MagicScroll(String name, String description, int power) {
+        super(name, description);
+        this.power = power;
+    }
+
+    @Override
+    public void use(Character character) {
+        //Maybe A free skill use with no mana cost
+        System.out.println("Using " + name + ", effect power: " + power);
+    }
+    public String getName(){
+        return super.name;
+    }
+    public String getDescription(){
+        return super.description;
+    }
 }
