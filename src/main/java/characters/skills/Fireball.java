7b07a293496170cc0507ef2c37652c8f453dc429
Your Name <djdemers@asu.edu>
2024-07-02 15:27:23 -0700

    added some rough skills

--- src/main/java/characters/skills/Fireball.java
+++ src/main/java/characters/skills/Fireball.java
@@ -1,2 +1,36 @@
-package characters.skills;public class Fireball {
+package characters.skills;
+
+import characters.Character;
+import characters.Mage;
+
+public class Fireball extends Skill {
+    private int damage;
+
+    public Fireball() {
+        super("Fireball", "A fiery explosive projectile that deals damage.", 3,20, 1);
+        this.damage = 50;
+    }
+
+    @Override
+    public void activate(Character caster, Character target) {
+        if (!(caster instanceof Mage)) {
+            System.out.println("This skill is only available to Mages.");
+            return;
+        }
+        if (isAvailable(caster)) {
+            applyBoost(caster);
+        } else {
+            System.out.println("Not enough mana or skill is on cooldown.");
+        }
+    }
+    private void applyBoost(Character caster) {
+        caster.setAttackPower(caster.getAttackPower() + damage);
+        System.out.println(caster.getName() + " uses " + getName() + ", increasing attack power by " + damage + ".");
+    }
+
+    @Override
+    public void deactivate(Character caster) {
+        caster.setAttackPower(caster.getAttackPower() - damage);
+        System.out.println(getName() + " effect ends, decreasing " + caster.getName() + "'s attack power by " + damage + ".");
+    }
 }
--- src/main/java/characters/skills/Rage.java
+++ src/main/java/characters/skills/Rage.java
@@ -1,2 +1,37 @@
-package characters.skills;public class Rage {
+package characters.skills;
+
+import characters.Character;
+import characters.Warrior;
+
+public class Rage extends Skill {
+    private int attackBoost;
+
+    public Rage() {
+        super("Rage", "Temporarily boosts attack power.", 1, 5, 2);
+        this.attackBoost = 5;
+    }
+
+    @Override
+    public void activate(Character caster, Character target) {
+        if (!(caster instanceof Warrior)) {
+            throw new IllegalArgumentException("Rage can only be activated by Warriors.");
+        }
+        if (isAvailable(caster)) {
+            applyBoost(caster);
+        } else {
+            throw new IllegalStateException("Skill not available or not enough resources.");
+        }
+    }
+
+
+    private void applyBoost(Character caster) {
+        caster.setAttackPower(caster.getAttackPower() + attackBoost);
+        System.out.println(caster.getName() + " uses " + getName() + ", increasing attack power by " + attackBoost + ".");
+    }
+
+    @Override
+    public void deactivate(Character caster) {
+        caster.setAttackPower(caster.getAttackPower() - attackBoost);
+        System.out.println(getName() + " effect ends, decreasing " + caster.getName() + "'s attack power by " + attackBoost + ".");
+    }
 }
