ecd10e857cab938854856016a04baf11781f4435
Your Name <djdemers@asu.edu>
2024-07-02 15:35:56 -0700

    added rough Effect class

--- src/main/java/enemies/effects/Effect.java
+++ src/main/java/enemies/effects/Effect.java
@@ -1,2 +1,49 @@
-package enemies.effects;public class Effect {
+package enemies.effects;
+
+import characters.Character;
+public class Effect {
+    private int duration;
+    private Runnable effectApply;
+    private Runnable effectRemove;
+
+    public Effect(int duration, Runnable effectApply, Runnable effectRemove) {
+        this.duration = duration;
+        this.effectApply = effectApply;
+        this.effectRemove = effectRemove;
+    }
+
+    /**
+     * Decreases the duration of the effect by one unit.
+     * Typically called once per turn or game cycle.
+     */
+    public void decreaseDuration() {
+        if (duration > 0) {
+            duration--;
+        }
+    }
+
+    /**
+     * Applies the effect's action to the character.
+     * This should be called when the effect is first added.
+     */
+    public void applyEffect(Character character) {
+        effectApply.run();
+    }
+
+    /**
+     * Removes the effect's action from the character.
+     * This should be called when the effect expires.
+     */
+    public void removeEffect(Character character) {
+        effectRemove.run();
+    }
+
+    /**
+     * Checks if the effect has expired.
+     * @return true if the duration has reached 0 or less, false otherwise.
+     */
+    public boolean isExpired() {
+        return duration <= 0;
+    }
 }
+
