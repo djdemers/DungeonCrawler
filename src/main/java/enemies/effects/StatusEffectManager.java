15d51845bc986e2e53a64db791509c7ea6d0b8e5
Your Name <djdemers@asu.edu>
2024-07-02 15:36:49 -0700

    added rough StatusEffectManager

--- src/main/java/enemies/effects/StatusEffectManager.java
+++ src/main/java/enemies/effects/StatusEffectManager.java
@@ -1,2 +1,32 @@
-package enemies.effects;public class StatusEffectManager {
+package enemies.effects;
+
+import characters.Character;
+
+import java.util.*;
+
+public class StatusEffectManager {
+    private Map<Character, List<Effect>> effects = new HashMap<>();
+
+    public void applyEffect(Character character, Effect effect) {
+        effects.computeIfAbsent(character, k -> new ArrayList<>()).add(effect);
+        effect.applyEffect(character);
+    }
+
+    public void updateEffects(Character character) {
+        List<Effect> characterEffects = effects.get(character);
+        if (characterEffects != null) {
+            Iterator<Effect> it = characterEffects.iterator();
+            while (it.hasNext()) {
+                Effect effect = it.next();
+                effect.decreaseDuration();
+                if (effect.isExpired()) {
+                    effect.removeEffect(character);
+                    it.remove();
+                }
+            }
+        }
+    }
+
+
 }
+
