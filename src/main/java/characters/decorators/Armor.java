0f91be4a2729c2377d6a5b54473381e8e08b4213
djdemers <djdemers@asu.edu>
2024-07-03 16:39:49 -0700

    added javadoc and fixed checkstyle errors

--- src/main/java/characters/decorators/Armor.java
+++ src/main/java/characters/decorators/Armor.java
@@ -1,48 +1,102 @@
 package characters.decorators;
 
 import characters.Character;
-import service.Equipable;
+import interfaces.Equipable;
 
+/**
+ * Represents armor that can be equipped by a character to increase defense.
+ * This class extends CharacterDecorator, allowing it to modify character attributes dynamically.
+ */
 public class Armor extends CharacterDecorator implements Equipable {
     private String itemName;
     private int additionalDefense;
+    private int price;
 
-    public Armor(String name, Character decoratedCharacter, int additionalDefense) {
+    /**
+     * Constructs an Armor object that decorates a character, enhancing its defense.
+     *
+     * @param name the name of the armor
+     * @param decoratedCharacter the character to be decorated
+     * @param additionalDefense the additional defense provided by the armor
+     * @param price the cost of the armor
+     */
+    public Armor(String name, Character decoratedCharacter, int additionalDefense, int price) {
         super(decoratedCharacter);
         this.itemName = name;
         this.additionalDefense = additionalDefense;
+        this.price = price;
     }
+
+    /**
+     * Equips the armor to the character, applying additional defense attributes.
+     */
     @Override
     public CharacterDecorator equip(Character character) {
-       this.apply();
-        System.out.println(character.getName() + " equips " + itemName + " and gains " + additionalDefense + " defense.");
-       return this;
+        this.apply();
+        System.out.println("equipped " + itemName + " and gained " + additionalDefense + " defense.");
+        return this;
     }
+
+    /**
+     * Unequips the armor from the character, removing the previously applied defense enhancements.
+     */
     @Override
     public void unequip(Character character) {
         this.revert();
-        System.out.println(character.getName() + " unequips " + itemName);
+        System.out.println("unequips " + itemName);
     }
+
+    /**
+     * Applies the defensive enhancement to the character.
+     */
     @Override
     public void apply() {
         decoratedCharacter.setDefense(decoratedCharacter.getDefense() + additionalDefense);
     }
+
+    /**
+     * Reverts the defensive enhancement from the character.
+     */
     @Override
     public void revert() {
         decoratedCharacter.setDefense(decoratedCharacter.getDefense() - additionalDefense);
     }
 
-    public String getName(){
+    /**
+     * Gets the name of the item.
+     *
+     * @return the name of the item.
+     */
+    public String getName() {
         return itemName;
     }
 
+    /**
+     * Gets the type of the item.
+     *
+     * @return the type of the item, which is "Armor".
+     */
     @Override
-    public String getType(){
+    public String getType() {
         return "Armor";
     }
+
+    /**
+     * Gets the bonus provided by the item.
+     *
+     * @return the additional defense provided by the armor.
+     */
     @Override
-    public int getBonus(){
+    public int getBonus() {
         return additionalDefense;
     }
 
+    /**
+     * Gets the price of the item.
+     *
+     * @return the price of the item.
+     */
+    public int getPrice() {
+        return price;
+    }
 }
