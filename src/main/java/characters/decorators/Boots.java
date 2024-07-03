1daf34c562438a6650876271267845e323fb0195
djdemers <djdemers@asu.edu>
2024-07-03 16:40:11 -0700

    added javadoc and fixed checkstyle errors

--- src/main/java/characters/decorators/Boots.java
+++ src/main/java/characters/decorators/Boots.java
@@ -1,48 +1,103 @@
 package characters.decorators;
 
 import characters.Character;
-import service.Equipable;
+import interfaces.Equipable;
 
+/**
+ * Represents boots that can be equipped by a character to increase speed.
+ * This class extends CharacterDecorator, allowing it to modify character attributes dynamically.
+ */
 public class Boots extends CharacterDecorator implements Equipable {
     private String itemName;
     private int additionalSpeed;
+    private int price;
 
-    public Boots(String name, Character decoratedCharacter, int additionalSpeed) {
+    /**
+     * Constructs a Boots object that decorates a character, enhancing its speed.
+     *
+     * @param name the name of the boots
+     * @param decoratedCharacter the character to be decorated
+     * @param additionalSpeed the additional speed provided by the boots
+     * @param price the cost of the boots
+     */
+    public Boots(String name, Character decoratedCharacter, int additionalSpeed, int price) {
         super(decoratedCharacter);
         this.itemName = name;
         this.additionalSpeed = additionalSpeed;
-
+        this.price = price;
     }
+
+    /**
+     * Equips the boots to the character, applying additional speed.
+     */
     @Override
     public CharacterDecorator equip(Character character) {
-        this.apply();  // Applies this decorator's effects
-        System.out.println(character.getName() + " equips " + itemName + " and gains " + additionalSpeed + " speed.");
+        this.apply();
+        System.out.println("equipped " + itemName + " and gained " + additionalSpeed + " speed.");
         return this;
     }
+
+    /**
+     * Unequips the boots from the character, removing the previously applied speed enhancements.
+     */
     @Override
     public void unequip(Character character) {
-        this.revert();  // Reverts this decorator's effects
-        System.out.println(character.getName() + " unequips " + itemName);
+        this.revert();
+        System.out.println("unequips " + itemName);
     }
+
+    /**
+     * Applies the speed enhancement to the character.
+     */
     @Override
-    public void apply(){
+    public void apply() {
         decoratedCharacter.setSpeed(decoratedCharacter.getSpeed() + additionalSpeed);
     }
+
+    /**
+     * Reverts the speed enhancement from the character.
+     */
     @Override
-    public void revert(){
+    public void revert() {
         decoratedCharacter.setSpeed(decoratedCharacter.getSpeed() - additionalSpeed);
     }
-    public String getName(){
+
+    /**
+     * Gets the name of the item.
+     *
+     * @return the name of the boots.
+     */
+    public String getName() {
         return itemName;
     }
+
+    /**
+     * Gets the type of the item.
+     *
+     * @return the type of the item, which is "Boots".
+     */
     @Override
-    public String getType(){
+    public String getType() {
         return "Boots";
     }
+
+    /**
+     * Gets the bonus provided by the item.
+     *
+     * @return the additional speed provided by the boots.
+     */
     @Override
-    public int getBonus(){
+    public int getBonus() {
         return additionalSpeed;
     }
 
+    /**
+     * Gets the price of the item.
+     *
+     * @return the price of the boots.
+     */
+    public int getPrice() {
+        return price;
+    }
 }
 
--- src/main/java/characters/decorators/Sword.java
+++ src/main/java/characters/decorators/Sword.java
@@ -1,51 +1,102 @@
 package characters.decorators;
 
 import characters.Character;
-import service.Equipable;
+import interfaces.Equipable;
 
+/**
+ * Represents a sword that can be equipped by a character to increase attack power.
+ * This class extends CharacterDecorator, allowing it to modify character attributes dynamically.
+ */
 public class Sword extends CharacterDecorator implements Equipable {
     private String itemName;
     private int attackBoost;
+    private int price;
 
-    public Sword(String name, Character decoratedCharacter, int attackBoost) {
+    /**
+     * Constructs a Sword object that decorates a character, enhancing its attack power.
+     *
+     * @param name the name of the sword
+     * @param decoratedCharacter the character to be decorated
+     * @param attackBoost the additional attack power provided by the sword
+     * @param price the cost of the sword
+     */
+    public Sword(String name, Character decoratedCharacter, int attackBoost, int price) {
         super(decoratedCharacter);
         this.itemName = name;
         this.attackBoost = attackBoost;
+        this.price = price;
     }
+
+    /**
+     * Equips the sword to the character, applying additional attack power.
+     */
     @Override
     public CharacterDecorator equip(Character character) {
         this.apply();
-        System.out.println(character.getName() + " equips " + itemName + " and gains " + attackBoost + " attack.");
+        System.out.println("equipped " + itemName + " and gained " + attackBoost + " attack.");
         return this;
     }
+
+    /**
+     * Unequips the sword from the character, removing the previously applied attack enhancements.
+     */
     @Override
     public void unequip(Character character) {
-        this.revert();  // Reverts this decorator's effects
-        System.out.println(character.getName() + " unequips " + itemName);
+        this.revert();
+        System.out.println("unequips " + itemName);
     }
+
+    /**
+     * Applies the attack power enhancement to the character.
+     */
     @Override
     public void apply() {
         decoratedCharacter.setAttackPower(decoratedCharacter.getAttackPower() + attackBoost);
     }
+
+    /**
+     * Reverts the attack power enhancement from the character.
+     */
     @Override
     public void revert() {
         decoratedCharacter.setAttackPower(decoratedCharacter.getAttackPower() - attackBoost);
     }
-    public String getName(){
+
+    /**
+     * Gets the name of the item.
+     *
+     * @return the name of the sword.
+     */
+    public String getName() {
         return itemName;
     }
 
+    /**
+     * Gets the type of the item.
+     *
+     * @return the type of the item, which is "Sword".
+     */
     @Override
-    public String getType(){
+    public String getType() {
         return "Sword";
     }
+
+    /**
+     * Gets the bonus provided by the item.
+     *
+     * @return the additional attack power provided by the sword.
+     */
     @Override
-    public int getBonus(){
+    public int getBonus() {
         return attackBoost;
     }
 
+    /**
+     * Gets the price of the item.
+     *
+     * @return the price of the sword.
+     */
+    public int getPrice() {
+        return price;
+    }
 }
-
-
-
-
