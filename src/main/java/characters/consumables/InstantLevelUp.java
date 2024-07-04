54f3d084cb0f1bffceea274ec0d7636ecf1d7574
djdemers <djdemers@asu.edu>
2024-07-03 18:59:54 -0700

    added javadoc and fixed checkstyle errors

--- src/main/java/characters/consumables/InstantLevelUp.java
+++ src/main/java/characters/consumables/InstantLevelUp.java
@@ -1,21 +1,66 @@
 package characters.consumables;
 
 import characters.Character;
-import service.SingleUse;
+import interfaces.SingleUse;
 
+/**
+ * Represents a consumable item that instantly increases the character's level
+ * when used. This is a powerful item intended for rapid progression.
+ */
 public class InstantLevelUp extends Consumable implements SingleUse {
-    public InstantLevelUp(String name, String description, int levelIncrement){
+    private int price;
+    private int levelIncrement;
+
+    /**
+     * Constructs a new InstantLevelUp item.
+     *
+     * @param name The name of the item.
+     * @param description A brief description of the item's effects.
+     * @param levelIncrement The number of levels the character will gain upon using this item.
+     * @param price The price of the item if bought or sold.
+     */
+    public InstantLevelUp(String name, String description, int levelIncrement, int price) {
         super(name, description);
+        this.levelIncrement = levelIncrement;
+        this.price = price;
     }
+
+    /**
+     * Applies the effect of the item to the specified character, increasing their level.
+     * Currently, the actual level-up logic is commented out and needs implementation.
+     *
+     * @param character The character who uses the item and benefits from its effects.
+     */
     @Override
-    public void use(Character character){
-        //character.levelUp();
+    public void use(Character character) {
+        // character.levelUp(); // Uncomment and implement this method in the Character class to actually increase the level.
         System.out.println(character.getName() + " uses " + name + " and levels up instantly!");
     }
-    public String getName(){
+
+    /**
+     * Returns the name of the item.
+     *
+     * @return The name of the item.
+     */
+    public String getName() {
         return super.name;
     }
-    public String getDescription(){
+
+    /**
+     * Returns the price of the item.
+     *
+     * @return The price of the item.
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * Returns the description of the item.
+     *
+     * @return The description of the item.
+     */
+    public String getDescription() {
         return super.description;
     }
 }
