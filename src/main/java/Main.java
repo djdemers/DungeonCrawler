1c9d3cad02e916cbe857a8e527bd813783ec6d00
djdemers <djdemers@asu.edu>
2024-07-03 20:00:07 -0700

    minor adjustments to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,25 @@
 import characters.Character;
-import characters.decorators.CharacterDecorator;
-import characters.decorators.Sword;
-import enemies.Enemy;
+
+import enemies.EnemyLeveling;
 import factories.CharacterFactory;
-import factories.EnemyFactory;
-import inventory.Inventory;
-import inventory.Shop;
-import service.Item;
+import interfaces.EnemyScalingStrategy;
 
 public class Main {
 
     public static void main(String[] args) {
         Main app = new Main();
         System.out.println(app.getGreeting());
+
         // Create a character using a factory method
+        EnemyScalingStrategy scalingStrategy = new EnemyLeveling();
         Character hero = CharacterFactory.createRandomCharacter("Hero");
         System.out.println(hero.getClass().getSimpleName() + " is the class you have been assigned");
         System.out.println(hero.getRace().getName() + " is the race that you have been assigned");
 
-        //Initial Stats
-        //System.out.println("Before any decoration:");
-        //hero.displayStats();
-
-        // Set up the inventory
-        //Inventory heroInventory = hero.getInventory();
-
-
-        // Create a shop and display its items
-        //Shop shop = new Shop(hero);
-        //shop.displayItems();  // Display items available for sale
-
-        // Example of buying an item from the shop
-        //if (!shop.getItemsForSale().isEmpty()) {
-        //    Item itemToBuy = shop.getItemsForSale().get(0);
-        //    shop.buyItem(hero, itemToBuy);
-        //    System.out.println("After buying an item:");
-        //    heroInventory.listItems();
-        //}
-
-
-
-        // Display stats after equipping
-        //System.out.println("After using items and equipping:");
-        //hero.displayStats();
 
         // Example combat scenario
-        GameMediator mediator = new GameMediatorImpl(hero);
-        Enemy initialEnemy = EnemyFactory.createRandomSmallEnemy();
-        mediator.fight(hero, initialEnemy);
+        GameMediator mediator = new GameMediatorImpl(hero, scalingStrategy);
+        mediator.nextLevel();
     }
     public String getGreeting() {
         return "Welcome to the Dungie!";
