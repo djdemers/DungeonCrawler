06bed3593146284ca1142664a9a6691e1569c645
djdemers <djdemers@asu.edu>
2024-07-03 19:43:51 -0700

    added javadoc and fixed checkstyle errors

--- src/main/java/factories/EnemyFactory.java
+++ src/main/java/factories/EnemyFactory.java
@@ -1,62 +1,93 @@
 package factories;
 
 import enemies.*;
+import interfaces.EnemyScalingStrategy;
 
 /**
  * Factory class for creating enemies. This class abstracts the instantiation
- * of different types of enemies based on the specified enemy type.
+ * of different types of enemies based on the specified enemy type and scales
+ * them according to a given strategy.
  */
 public class EnemyFactory {
 
+    private EnemyScalingStrategy scalingStrategy;
+
+    public EnemyFactory(EnemyScalingStrategy scalingStrategy) {
+        this.scalingStrategy = scalingStrategy;
+    }
+
     /**
-     * Creates an enemy of the specified type.
+     * Creates an enemy of the specified type and applies scaling based on the floor number.
      *
      * @param type The type of enemy to create.
-     * @return The newly created enemy object.
+     * @param currentFloor The current floor number to base the scaling on.
+     * @return The newly created and scaled enemy object.
      * @throws IllegalArgumentException if the provided type is not recognized.
      */
-    public static Enemy createEnemy(EnemyType type) {
+    public Enemy createEnemy(EnemyType type, int currentFloor) {
+        Enemy enemy;
         switch (type) {
             case GOBLIN:
-                return new Goblin();
+                enemy = new Goblin();
+                break;
             case RAT:
-                return new Rat();
+                enemy = new Rat();
+                break;
             case SPIDER:
-                return new Spider();
+                enemy = new Spider();
+                break;
             case TROLL:
-                return new Troll();
+                enemy = new Troll();
+                break;
+            case BEAR:
+                enemy = new Bear();
+                break;
+            case WRAITH:
+                enemy = new Wraith();
+                break;
+            case GIANT:
+                enemy = new Giant();
+                break;
             case DRAGON:
-                return new Dragon();
+                enemy = new Dragon();
+                break;
+            case SWORDMASTER:
+                enemy = new SwordMaster();
+                break;
             default:
                 throw new IllegalArgumentException("Unknown enemy type: " + type);
         }
+        scalingStrategy.scaleEnemyAttributes(enemy, currentFloor);
+        return enemy;
     }
 
     /**
-     * Creates a random small enemy.
+     * Creates a random small enemy and applies scaling.
      *
+     * @param currentFloor The current floor number to base the scaling on.
      * @return A new instance of a random small enemy.
      */
-    public static Enemy createRandomSmallEnemy() {
-        return createEnemy(EnemyType.getRandomSmallEnemy());
+    public Enemy createRandomSmallEnemy(int currentFloor) {
+        return createEnemy(EnemyType.getRandomSmallEnemy(), currentFloor);
     }
 
     /**
-     * Creates a random medium enemy.
+     * Creates a random medium enemy and applies scaling.
      *
+     * @param currentFloor The current floor number to base the scaling on.
      * @return A new instance of a random medium enemy.
      */
-    public static Enemy createRandomMediumEnemy() {
-        return createEnemy(EnemyType.getRandomMediumEnemy());
+    public Enemy createRandomMediumEnemy(int currentFloor) {
+        return createEnemy(EnemyType.getRandomMediumEnemy(), currentFloor);
     }
 
     /**
-     * Creates a random boss enemy.
+     * Creates a random boss enemy and applies scaling.
      *
+     * @param currentFloor The current floor number to base the scaling on.
      * @return A new instance of a random boss enemy.
      */
-    public static Enemy createRandomBoss() {
-        return createEnemy(EnemyType.getRandomBossEnemy());
+    public Enemy createRandomBoss(int currentFloor) {
+        return createEnemy(EnemyType.getRandomBossEnemy(), currentFloor);
     }
 }
-
